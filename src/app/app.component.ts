e1dcfd2d787e3e77731580de28d764db35513810
JWord <jwren4170@att.net>
2021-11-25 11:18:03 -0600

    Typing test project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(e: string) {
+    this.enteredText = e;
+  }
+
+  compare(randLetter: string, enteredLetter: string) {
+    return !enteredLetter
+      ? 'pending'
+      : randLetter === enteredLetter
+      ? 'green'
+      : 'red';
+  }
 }
